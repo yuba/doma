49a04e8e59b3789f66da0c82d2e36bb1a5ae2bf0
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-03-14 22:12:59 +0900

    Ignore NoSuchFileException, because it is thrown instead of FileNotFoundException

--- src/test/java/org/seasar/aptina/unit/TestingJavaFileManager.java
+++ src/test/java/org/seasar/aptina/unit/TestingJavaFileManager.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.nio.file.NoSuchFileException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.tools.FileObject;
@@ -131,6 +132,7 @@ class TestingJavaFileManager extends ForwardingJavaFileManager<StandardJavaFileM
       uri = originalFileObject.toUri();
       content = IOUtils.readBytes(originalFileObject.openInputStream());
     } catch (final FileNotFoundException ignore) {
+    } catch (final NoSuchFileException ignore) {
     }
     final InMemoryJavaFileObject fileObject =
         new InMemoryJavaFileObject(
